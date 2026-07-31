134de41972d3f36edd3fd73d8441b8c5da46e94b
dim-ghub <mlrudasill@gmail.com>
2026-07-31 00:38:17 -0500

    Add current multiplier text to speed actions

--- actions/SpeedUpAction/SpeedUpAction.py
+++ actions/SpeedUpAction/SpeedUpAction.py
@@ -1,4 +1,5 @@
 from ..PlaybackAction import PlaybackAction
+import json
 
 class SpeedUpAction(PlaybackAction):
     def on_ready(self) -> None:
@@ -6,3 +7,13 @@ class SpeedUpAction(PlaybackAction):
         
     def on_key_down(self) -> None:
         self.send_command("SPEED_UP")
+
+    def on_tick(self) -> None:
+        raw_json = self.send_command("GET_TIME")
+        if not raw_json: return
+        try:
+            data = json.loads(raw_json.strip())
+            spd = data.get("speed", 1.0)
+            self.set_bottom_label(f"{spd:.2f}x", [255, 255, 255, 255])
+        except:
+            pass
