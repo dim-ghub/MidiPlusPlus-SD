5b2577e0477a7cd46dbca6dd8184dcab59e0aedb
dim-ghub <mlrudasill@gmail.com>
2026-07-31 00:42:03 -0500

    Use image assets for speed actions instead of text labels

--- actions/SpeedDownAction/SpeedDownAction.py
+++ actions/SpeedDownAction/SpeedDownAction.py
@@ -1,19 +1,15 @@
+import os
+import globals as gl
+
 from ..PlaybackAction import PlaybackAction
-import json
 
 class SpeedDownAction(PlaybackAction):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.has_custom_image_fallback = True
+
     def on_ready(self) -> None:
-        pass
+        self.set_media(media_path=os.path.join(gl.plugins_path, "com_midiplusplus_Controller", "assets", "fast_rewind.png"), size=0.8)
         
     def on_key_down(self) -> None:
         self.send_command("SPEED_DOWN")
-
-    def on_tick(self) -> None:
-        raw_json = self.send_command("GET_TIME")
-        if not raw_json: return
-        try:
-            data = json.loads(raw_json.strip())
-            spd = data.get("speed", 1.0)
-            self.set_bottom_label(f"{spd:.2f}x", [255, 255, 255, 255])
-        except:
-            pass
