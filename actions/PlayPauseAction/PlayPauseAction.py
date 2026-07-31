4cf545fb2839b5de0bc938b15f686d47f51794c7
dim-ghub <mlrudasill@gmail.com>
2026-07-31 00:47:53 -0500

    Add on_tick to dynamically swap play and pause icons based on state

--- actions/PlayPauseAction/PlayPauseAction.py
+++ actions/PlayPauseAction/PlayPauseAction.py
@@ -1,11 +1,30 @@
 from ..PlaybackAction import PlaybackAction
 
 import os
+import json
 
 class PlayPauseAction(PlaybackAction):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.has_custom_image_fallback = True
+
     def on_ready(self) -> None:
-        icon_path = os.path.join(self.plugin_base.PATH, "assets", "play.png")
-        self.set_media(media_path=icon_path, size=0.85)
+        self.update_icon(True)
         
+    def update_icon(self, is_paused: bool):
+        filename = "play.png" if is_paused else "pause.png"
+        icon_path = os.path.join(self.plugin_base.PATH, "assets", filename)
+        self.set_media(media_path=icon_path, size=0.85)
+
     def on_key_down(self) -> None:
         self.send_command("PLAY_PAUSE")
+
+    def on_tick(self) -> None:
+        raw_json = self.send_command("GET_TIME")
+        if not raw_json: return
+        try:
+            data = json.loads(raw_json.strip())
+            is_paused = data.get("paused", True)
+            self.update_icon(is_paused)
+        except:
+            pass
